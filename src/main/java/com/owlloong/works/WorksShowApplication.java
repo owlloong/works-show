e2427ffe9316c1ee522ebf237ee694929d600336
yuanjinchuan <yuanjinchuan@szboanda.net>
2021-05-02 17:15:00 +0800

    Added by owlloong:初始化提交(Initial Commit)

---
+++ src/main/java/com/owlloong/works/WorksShowApplication.java
@@ -0,0 +1,13 @@
+package com.owlloong.works;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorksShowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/owlloong/works/WorksShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owlloong.works;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorksShowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
